a2393a4f3340a4a768c7e92ffe8e3bc1fed18a4e
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-02-13 22:03:38 +0900

    list test

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import { useStore } from "../store";
 import RenderInput from "../components/RenderInput";
+import FrameworkList from "../components/FrameworkList";
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
@@ -9,10 +10,26 @@ export default function App({ Component, pageProps }) {
     console.log(text);
   };
 
+  const data = [
+    {
+      id: 1,
+      item: "React",
+    },
+    {
+      id: 2,
+      item: "Angular",
+    },
+    {
+      id: 3,
+      item: "Vue",
+    },
+  ];
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
       <RenderInput outputConsole={outputConsole}></RenderInput>
+      <FrameworkList frameworks={data} />
     </Provider>
   );
 }
